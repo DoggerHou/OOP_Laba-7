8dbaca674aa3fd82d22d2cc9725dd04ef6fc50ec
Артём Сурков <mr.slipa3@mail.ru>
2022-12-04 00:33:55 +0500

    Добавьте файлы проекта.

---
+++ OOP_Laba 7/FactoryMethod.cs
@@ -0,0 +1,34 @@
+﻿namespace OOP_Laba_7
+{
+    public abstract class ModelFactory //Абстрактная фабрика
+    {
+        public virtual Model CreateObject(string code) {return null;}
+    }
+
+
+    public class MyObjectsFactory : ModelFactory
+    {
+        public override Model CreateObject(string code)
+        {
+            Model temp = null;
+            switch (code)
+            {
+                case "Circle":
+                    temp = new CCircle();
+                    break;
+                case "Square":
+                    temp = new CSquare();
+                    break;
+                case "Triangle":
+                    temp = new Triangle();
+                    break;
+                case "Group":
+                    temp = new Group();
+                    break;
+            }
+            return temp;
+        }
+    }
+}
+
+   
